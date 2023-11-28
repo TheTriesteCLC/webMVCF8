25caea7443b4d805cec30d61c78d9511809a4178
Vu Doan <khanhdoannt@gmail.com>
2023-11-28 20:05:22 +0700

    Create checking login and signup account done

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -3,6 +3,36 @@ const { multipleMongooseToObject } = require('../../util/mongoose')
 const { mongooseToObject } = require('../../util/mongoose')
 
 class userController {
+
+    //[GET] /log-in
+    login(req, res, next) {
+        res.render('users/login');
+    }
+
+    //[GET] /sign-up
+    signup(req, res, next) {
+        res.render('users/signup');
+    }
+
+    //[POST] /create
+    create(req, res, next) {
+        const formData = req.body;
+        formData.slug = 'user-' + formData.username;
+        const user = new User(formData);
+        user.save();
+        res.render('users/welcome', { formData });
+    }
+
+    //[POST] /find
+    find(req, res, next) {
+        const formData = req.body;
+        User.find({ username: formData.username, password: formData.password })
+            .then(users => {
+                res.render('home', { users: multipleMongooseToObject(users) });
+            })
+            .catch(error => next(error));
+    }
+
     //[GET] /user/:slug
     show(req, res, next) {
         User.findOne({ slug: req.params.slug })
@@ -12,24 +42,16 @@ class userController {
             .catch(next)
     }
 
-    //[GET] /user/log-in
-    login(req, res, next) {
-        res.render('users/login');
+    //[GET] /welcome
+    welcome(req, res) {
+        User.find({})
+            .then(users => {
+                res.render('home', { users: multipleMongooseToObject(users) });
+            })
+            .catch(error => next(error));
     }
 
-    //[GET] /user/sign-up
-    signup(req, res, next) {
-        res.render('users/signup');
-    }
 
-    //[POST] /user/store
-    store(req, res, next) {
-        const formData = req.body;
-        formData.slug = 'user-' + formData.username;
-        const user = new User(formData);
-        user.save();
-        res.send("Create new user !!!");
-    }
 }
 
 module.exports = new userController();
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const userController = require('../app/controllers/userController');
 
+router.get('/show', userController.show);
+router.get('/welcome', userController.welcome);
 router.get('/log-in', userController.login);
 router.get('/sign-up', userController.signup);
-router.post('/store', userController.store);
+router.post('/find', userController.find);
+router.post('/create', userController.create);
 router.get('/', userController.login);
 // router.get('/:slug', userController.show);
 
