0438a97bcd414ca60929dfaa064749cc883a8e39
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-28 20:53:17 +0700

    add passport, express-session

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ const morgan = require('morgan');
 const handleBars = require('express-handlebars');
 const path = require('path');
 const route = require('./routes');
+const passport = require('passport');
+const session = require('express-session');
 
 const db = require('./config/db');
 
 //Connect to DB
-const databaseUrl = 'mongodb+srv://vmtriet21:vmtriet21@ptudweb-ga02.dbulhp7.mongodb.net/PTUDWEB-GA02';
+// const databaseUrl = 'mongodb+srv://vmtriet21:vmtriet21@ptudweb-ga02.dbulhp7.mongodb.net/PTUDWEB-GA02';
+const databaseUrl = 'mongodb://127.0.0.1:27017/testFormDb';
 db.connect(databaseUrl);
 
 
@@ -33,6 +36,14 @@ app.use(
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//setup session
+app.use(session({ secret: '440457' }));
+
+// init passport
+require('./config/passport/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Route init
 route(app);
