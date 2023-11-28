0a58418ed5913d42e0a62de8058b6ba72d5725c1
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-28 13:46:32 +0700

    add passport, passport local

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const morgan = require('morgan');
 const handleBars = require('express-handlebars');
 const path = require('path');
 const route = require('./routes');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// pass passport for configuration
+require('./config/passport')(passport);
 
 const db = require('./config/db');
-
 //Connect to DB
 const databaseUrl = 'mongodb://localhost:27017/testFormDb';
 db.connect(databaseUrl);
-
 
 const app = express();
 const port = 3000;
@@ -34,8 +38,25 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// setup session
+const store = session.MemoryStore();
+app.use(session({
+    saveUninitialized: false,
+    secret: "440457",
+    cookie: {
+        maxAge: 1000 * 10 // 1s * 10
+    },
+    store
+}))
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 //Route init
 route(app);
+
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}/`);
