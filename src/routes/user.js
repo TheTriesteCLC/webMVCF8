c3a9583423678372ce589252a239094284b09c3c
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-12-10 14:42:47 +0700

    refractor handle section

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,16 +2,54 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 require('../config/passport/passport')(passport);
-const userController = require('../app/controllers/userController');
+const User = require('../app/models/User');
+const { multipleMongooseToObject } = require('../util/mongoose')
+const {mongooseToObject} = require('../util/mongoose')
+
+router.get('/sign-up', function(req, res, next){
+    res.render('users/signup');
+});
 
 
-router.get('/sign-up', userController.signup);
-router.get('/login', userController.login);
-router.post('/store', userController.store);
-router.get('/protected', isLoggedIn, userController.protected);
-router.post('/logingin', userController.logingin);
-router.get('/:slug', userController.show);
-router.get('/logout', userController.logout);
+router.get('/login', function(req, res, next){
+    res.render('users/login');
+});
+
+
+router.post('/store',
+    passport.authenticate('local-signup', {
+        successRedirect : './login', 
+        failureRedirect : './sign-up'
+    })
+);
+
+
+router.get('/protected', isLoggedIn, function(req, res, next){
+    res.render('users/protected');
+});
+
+
+router.post('/logingin', passport.authenticate('local-login', {
+    successRedirect : './protected', // redirect to the secure profile section
+    failureRedirect : './login'// redirect back to the signup page if there is an error
+}), function(err, req,res,next){
+    console.log(err);
+});
+
+
+router.get('/:slug', function(req, res, next){
+    User.findOne({slug: req.params.slug})
+    .then((user) => {
+        res.render('users/show', {user: mongooseToObject(user)})
+    })
+    .catch(next)
+});
+
+
+router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('user/login');
+});
 
 
 // route middleware to make sure a user is logged in
